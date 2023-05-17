06aa2d3935377cbb7fac6105105abf7d6a6d6c4e
Isha Popat <isha.popat@BLRETV-C02GKTJ6M.local>
2023-05-17 17:30:27 +0530

    sts commit

---
+++ src/com/flipkart/demo/Demo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author isha.popat
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("JAVA DEMO");
+
+	}
+
+}
